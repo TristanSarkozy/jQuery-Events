8e73f4f85832fb61ce8ff1a34942b966b7a78574
Suciu Dan <suciu_t_dan@yahoo.com>
2024-05-31 12:48:00 +0000

    Add img folder and photos content

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ $(document).ready(function() {
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
+    });
+    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
+    });
+    $("#stream3_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
-   });
+    });
 }); 
